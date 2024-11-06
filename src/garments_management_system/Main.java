b114eb27a245e35e40104da21832be9818de3a66
refatkhan <refatkhan897@gmail.com>
2024-11-06 23:29:30 +0600

    add constructor on garments section and print them

--- src/garments_management_system/Main.java
+++ src/garments_management_system/Main.java
@@ -3,10 +3,11 @@ package garments_management_system;
 
 public class Main {
         public static void main(String[] args) {
-        Garments g1 = new Garments();
+        Garments g1 = new Garments("245","ABC Garments","Work with goods","1450 sqr feet","Red",20,30);
        
-        double x = g1.calculateDiscountPrice(10);
-        System.out.println(x);
+        g1.displayGarmentsInformation();
+//        double x = g1.calculateDiscountPrice(10);
+//        System.out.println(x);
 
     }
 }
